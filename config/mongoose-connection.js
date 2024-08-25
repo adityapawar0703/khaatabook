f0044481296a81a878e8b8344dfbb148d0f29840
adityapawar0703 <adityapawar07m@gmail.com>
2024-08-25 14:27:21 +0530

    new

--- config/mongoose-connection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.MONGO_DB_URI).then(function(){
-    console.log("connected to mongoDB")
-})
- let db = mongoose.connection
-module.exports = db;
