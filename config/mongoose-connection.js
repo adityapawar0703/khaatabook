d1e9385e37c29ac79ddcb3b12879da547d814abc
Aditya Pawar <154061750+adityapawar0703@users.noreply.github.com>
2024-08-25 12:44:42 +0530

    Update mongoose-connection.js

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_DB_URI,{
-    useNewUrlParser: true,
-  useUnifiedTopology: true,
-  ssl: true,
-}).then(function(){
+mongoose.connect(process.env.MONGO_DB_URI).then(function(){
     console.log("connected to mongoDB")
 })
  let db = mongoose.connection
